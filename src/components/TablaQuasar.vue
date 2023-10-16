682fbd11e2cb1caf27e11849ec0311a7433cbf8d
unknown <erickgutierrez085@gmail.com>
2023-10-16 12:38:58 -0500

    Mejoras3

--- src/components/TablaQuasar.vue
+++ src/components/TablaQuasar.vue
@@ -12,31 +12,30 @@
           <q-card-section>
             <div class="text-h6">{{ titleModal }}</div>
           </q-card-section>
-          
+
           <q-card-section class="q-pt-none">
             Sucursal <br />
-            <q-input v-model="formData.sucursal" type="text"></q-input>
+            <q-input v-model="sucursalNueva" type="text"></q-input>
           </q-card-section>
           <q-card-section class="q-pt-none">
             Origen <br />
-            <q-input v-model="formData.origen" type="text"></q-input>
+            <q-input v-model="origenNuevo" type="text"></q-input>
           </q-card-section>
           <q-card-section class="q-pt-none">
             Destino <br />
-            <q-input v-model="formData.destino" type="text"></q-input>
+            <q-input v-model="destinoNuevo" type="text"></q-input>
           </q-card-section>
           <q-card-section class="q-pt-none">
             Fecha De Salida
-            <q-input v-model="formData.fechaSalida" type="date"></q-input>
+            <q-input v-model="fecha_salidaNueva" type="date"></q-input>
           </q-card-section>
-          
+
           <q-card-actions align="right" class="bg-white text-teal">
             <q-btn flat label="Cancelar" v-close-popup />
             <q-btn flat label="Aceptar" @click="agregarRuta" />
           </q-card-actions>
         </q-card>
       </q-dialog>
-      
     </div>
 
     <div class="q-pa-md">
@@ -59,51 +58,82 @@ import { ref, onMounted } from "vue";
 import { format } from "date-fns";
 let formData = ref({
   sucursal: "",
-  origen: "",
-  destino: "",
-  fechaSalida: ""
+  Origen: "",
+  Destino: "",
+  fecha_salida: "",
 });
+const sucursalNueva = ref("");
+const origenNuevo = ref("");
+const destinoNuevo = ref("");
+const fecha_salidaNueva = ref("");
 async function agregarRuta() {
+  const fechaActual = new Date();
+  const data = {
+    sucursal: sucursalNueva.value,
+    Origen: origenNuevo.value,
+    Destino: destinoNuevo.value,
+    fecha_salida: fecha_salidaNueva.value,
+    estado: 1,
+    createdAt: fechaActual.toISOString(),
+  };
   try {
-    const response = await axios.post("ruta/rutacrear", formData.value);
+    const response = await axios.post("ruta/rutacrear", data);
     if (response.status === 200) {
-      rows.value.push({
-        sucursal: formData.value.sucursal,
-        Origen: formData.value.Origen,
-        Destino: formData.value.Destino,
-        fecha_salida: formData.value.fecha_salida,
-       
-      });
-    
+      datos.value.push(data);
+      sucursalNueva.value = "";
+      origenNuevo.value = "";
+      destinoNuevo.value = "";
+      fecha_salidaNueva.value = "";
+     
       medium.value = false;
-    
-      formData.value = {
-        sucursal: "",
-        origen: "",
-        destino: "",
-        fechaSalida: ""
-      };
+      DatosRutaPush()
+      
+    }else{
+      console.log('Error en la solicitud HTTP:', response.status, response.statusText);
     }
   } catch (error) {
     console.error("Error al agregar ruta:", error);
+
+    if (error.response) {
+      console.log('Respuesta de error:', error.response.status, error.response.data);
+    }
   }
 }
+const datos = ref([]);
 let rows = ref([]);
 let colums = ref([]);
 let titleModal = ref("Nueva Ruta");
 const medium = ref(false);
 async function ObtenerDatos() {
+  const response = await axios.get("ruta/rutabusca");
+  const data = response.data;
+  rows.value = data.ruta;
+  colums.value = data.ruta;
 
-
-    const response = await axios.get("ruta/rutabusca");
-    const data = response.data
-    rows.value = data.ruta;
-    colums.value = data.ruta;
-
-    console.log(data);
-  
+  console.log(data);
 }
+async function DatosRutaPush() {
+  try {
+    const response = await axios.get(`ruta/rutabusca`);
+    const data = response.data;
 
+    if (data.ruta.length > 0) {
+      for (const ruta of data.ruta) {
+        datos.value.push({
+          sucursal: ruta.sucursal,
+          Origen: ruta.Origen,
+          Destino: ruta.Destino,
+          fecha_salida: ruta.fecha_salida,
+          estado: ruta.estado,
+          createdAt: ruta.createdAt,
+        });
+      }
+      rows.value = datos.value;
+    }
+  } catch (error) {
+    console.error("Error al obtener datos:", error);
+  }
+}
 const columns = [
   {
     name: "Sucursal",
@@ -153,6 +183,7 @@ const columns = [
 
 onMounted(() => {
   ObtenerDatos();
+  DatosRutaPush();
 });
 </script>
 
