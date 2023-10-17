59a643d9c33672743a4a32364612a1354e9593c3
miguerlfr <luismiguelvargasjaimes@gmail.com>
2023-10-17 16:37:56 -0500

    actualizacion2

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,35 @@
 <template>
-    <div class="body">
-<<<<<<< HEAD
-        <router-link to="/Vendedores">
-=======
-        <router-link to="/vendedores">
->>>>>>> a100a32dc71f51101991c1c8fb9d71066246651a
-
-            <q-btn push color="primary" label="Vendedores" style="text-decoration: none;"/>
-            
-        </router-link>
-        
-        
+    <div>
+        <q-card class="my-card">
+            <q-card-section class="superior">
+                <div class="text-h6">Login</div>
+            </q-card-section>
+            <q-card-actions vertical align="right" class="texto">
+                <input type="text" placeholder="Nombre">
+                <input type="text" placeholder="Contraseña">
+                <q-btn flat>Aceptar</q-btn>
+            </q-card-actions>
+        </q-card>
     </div>
-
 </template>
-<style scoped>
-.body{
-    height: 100vh;
+
+<style>
+body{
+background: -webkit-linear-gradient(bottom, #2dbd6e, #a6f77b);
+  background-repeat: no-repeat;
+}
+.my-card{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 60%;
+    padding: 12px 44px;
+}
+.texto{
     display: flex;
     align-items: center;
-    justify-content: center;
+}
+.bg-teal text-white{
+    background-color: transparent;
 }
 </style>
---
+++ src/components/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="body">
+        <router-link to="/Vendedores">
+
+            <q-btn push color="primary" label="Vendedores" style="text-decoration: none;"/>
+            
+        </router-link>
+        
+        
+    </div>
+
+</template>
+<style scoped>
+.body{
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
