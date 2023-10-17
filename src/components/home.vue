426b40dc2060cfc213e086d81d272f26e363a471
miguerlfr <luismiguelvargasjaimes@gmail.com>
2023-10-17 17:48:12 -0500

    actualizacion

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,49 +1,53 @@
 <template>
     <q-layout view="hHh lpR fFf">
-  
-      <q-header reveal elevated class="bg-primary text-white">
-        <q-toolbar>
-          <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-  
-          <q-toolbar-title>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-            </q-avatar>
-            Title
-          </q-toolbar-title>
-        </q-toolbar>
-      </q-header>
-  
-      <q-drawer v-model="leftDrawerOpen" side="left" overlay behavior="mobile" elevated>
-        <!-- drawer content -->
-      </q-drawer>
-  
-      <q-page-container>
-        <router-view />
-      </q-page-container>
-  
+
+        <q-header reveal elevated class="bg-primary text-white">
+            <q-toolbar>
+                <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+                <q-toolbar-title>
+                    <q-avatar>
+                        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+                    </q-avatar>
+                    Title
+                </q-toolbar-title>
+            </q-toolbar>
+        </q-header>
+
+        <q-drawer v-model="leftDrawerOpen" side="left" overlay behavior="mobile" elevated id="lateral">
+                <router-link to="/Vendedores">  <q-btn align="left" class="btn-fixed-width" color="primary" label="Vendedores" /> </router-link>
+            <router-link to="/Bus"><q-btn>Bus</q-btn> </router-link>
+        </q-drawer>
+
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+
     </q-layout>
-  </template>
+</template>
   
-  <script>
-  import { ref } from 'vue'
-  
-  export default {
-    setup () {
-      const leftDrawerOpen = ref(false)
-  
-      return {
-        leftDrawerOpen,
-        toggleLeftDrawer () {
-          leftDrawerOpen.value = !leftDrawerOpen.value
+<script>
+import { ref } from 'vue'
+
+export default {
+    setup() {
+        const leftDrawerOpen = ref(false)
+
+        return {
+            leftDrawerOpen,
+            toggleLeftDrawer() {
+                leftDrawerOpen.value = !leftDrawerOpen.value
+            }
         }
-      }
     }
-  }
-  </script>
+}
+</script>
 
 
 
 <style scoped>
-
+#lateral{
+    display: flex;
+    flex-direction: column;
+}
 </style>
