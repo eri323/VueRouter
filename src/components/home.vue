9eb58e74e3bc65f74bca28d74d55caefef19974f
unknown <erickgutierrez085@gmail.com>
2023-10-18 20:02:07 -0500

    dsfd

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,55 +1,76 @@
 <template>
   <q-layout view="hHh lpR fFf">
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-      <q-header reveal elevated class="bg-primary text-white">
-          <q-toolbar>
-              <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
 
-              <q-toolbar-title>
-                  <q-avatar>
-                      <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-                  </q-avatar>
-                  Title
-              </q-toolbar-title>
-          </q-toolbar>
-      </q-header>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      side="left"
+      overlay
+      behavior="mobile"
+      elevated
+      id="lateral"
+    >
+      <router-link to="/Vendedores">
+        <q-btn
+          align="left"
+          class="btn-fixed-width"
+          color="primary"
+          label="Vendedores"
+        />
+      </router-link>
+      <router-link to="/Bus">
+        <q-btn
+          align="left"
+          class="btn-fixed-width"
+          color="primary"
+          label="Buses"
+        />
+      </router-link>
+      <router-link to="/Ruta">
+        <q-btn
+          align="left"
+          class="btn-fixed-width"
+          color="primary"
+          label="Rutas"
+      /></router-link>
+      <router-link to="/Cliente">
+        <q-btn
+          align="left"
+          class="btn-fixed-width"
+          color="primary"
+          label="Clientes"
+      /></router-link>
+    </q-drawer>
 
-      <q-drawer v-model="leftDrawerOpen" side="left" overlay behavior="mobile" elevated id="lateral">
-        <router-link to="/Vendedores">  <q-btn align="left" class="btn-fixed-width" color="primary" label="Vendedores" /> </router-link>
-        <router-link to="/Bus"> <q-btn align="left" class="btn-fixed-width" color="primary" label="Buses" /> </router-link>
-        <router-link to="/Ruta"> <q-btn align="left" class="btn-fixed-width" color="primary" label="Rutas" /></router-link>
-        <router-link to="/Cliente"> <q-btn align="left" class="btn-fixed-width" color="primary" label="Clientes" /></router-link>
-    
-      </q-drawer>
-
-      <q-page-container>
-          <router-view />
-      </q-page-container>
-
+    <q-page-container>
+      <router-view />
+    </q-page-container>
   </q-layout>
 </template>
 
-<script>
-import { ref } from 'vue'
+<script setup>
+import { ref } from "vue";
 
-export default {
-  setup() {
-      const leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false);
 
-      return {
-          leftDrawerOpen,
-          toggleLeftDrawer() {
-              leftDrawerOpen.value = !leftDrawerOpen.value
-          }
-      }
-  }
-}
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+};
 </script>
 
-
-
 <style scoped>
-#lateral{
+#lateral {
   display: flex;
   flex-direction: column;
 }
