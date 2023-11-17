f810aeaf86edab18a28428e591c23cf0a9f9ef09
unknown <erickgutierrez085@gmail.com>
2023-11-17 09:58:00 -0500

    ssss

--- src/components/tickete.vue
+++ src/components/tickete.vue
@@ -1,208 +1,374 @@
 <template>
-    <div>
-        <!-- Modal -->
-        <q-dialog v-model="fixed">
-            <q-card class="modal-content">
-                <q-card-section class="row items-center q-pb-none" style="color: black;">
+  <div>
+    <!-- Modal -->
+    <q-dialog v-model="fixed">
+      <q-card class="modal-content">
+        <q-card-section class="row items-center q-pb-none" style="color: black">
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+        <q-separator />
 
-                    <q-space />
-                    <q-btn icon="close" flat round dense v-close-popup />
-                </q-card-section>
-                <q-separator />
-
-                <q-card-section style="max-height: 50vh" class="scroll">
-                    <q-input v-model="Vehiculo" label="Numero ticket" style="width: 300px" />
-                    <q-input v-model="NumAsientos" label="Tipo venta" type="number" style="width: 300px;" />
-                  
-
-
-
-                    <div class="q-gutter-y-md column" style="max-width: 300px">
-                        <q-select clearable filled color="primary" v-model="conductor_id" :options="options"
-                            label="Conductor" />
-                    </div>
-
-                </q-card-section>
-
-                <q-separator />
-
-                <q-card-actions align="right">
-                    <q-btn flat label="Cancelar" color="primary" v-close-popup />
-                    <q-btn flat label="Aceptar" color="primary" @click="editarAgregarBus()" />
-                </q-card-actions>
-            </q-card>
-        </q-dialog>
-        <div>
-
-            <div class="btn-agregar">
-                <q-btn class="bg-secondary" label="Agregar Bus" @click="agregarBus()" />
+        <q-card-section style="max-height: 50vh" class="scroll">
+          <div class="q-pa" style="width: 300px">
+            <q-input
+              v-model="Nmro_ticket"
+              type="text"
+              label="Numero Asiento"
+              style="width: 300px"
+            />
+            <q-input
+              v-model="fecha_venta"
+              type="date"
+              label="Fecha de Venta"
+              style="width: 300px"
+            />
+            <div class="q-gutter">
+              <q-select
+                v-model="Vendedor_id"
+                :options="optionsVendedor"
+                label="Vendedor"
+              />
             </div>
-            <q-table title="Buses" :rows="rows" :columns="columns" row-key="name">
-                <template v-slot:body-cell-estado="props">
-                    <q-td :props="props">
-                        <label for="" v-if="props.row.estado == 1" style="color: green;">Activo</label>
-                        <label for="" v-else style="color: red;">Inactivo</label>
-                    </q-td>
-                </template>
-                <template v-slot:body-cell-opciones="props">
-                    <q-td :props="props" class="botones">
-                        <q-btn color="white" text-color="black" label="🖋️" @click="EditarBus(props.row._id)" />
-                        <q-btn glossy label="❌" @click="InactivarBus(props.row._id)" v-if="props.row.estado == 1" />
-                        <q-btn glossy label="✔️" @click="ActivarBus(props.row._id)" v-else />
-                    </q-td>
-                </template>
-            </q-table>
-        </div>
+          </div>
+          <div class="q-pa" style="width: 300px">
+            <div class="q-gutter">
+              <q-select
+                v-model="Cliente_id"
+                :options="optionsCliente"
+                label="Cliente"
+              />
+            </div>
+          </div>
+          <div class="q-pa" style="width: 300px">
+            <div class="q-gutter">
+              <q-select
+                v-model="Ruta_id"
+                :options="optionsRutas"
+                label="Rutas"
+              />
+            </div>
+          </div>
+          <div class="q-pa" style="width: 300px">
+            <div class="q-gutter">
+              <q-select
+                v-model="Transporte_id"
+                :options="optionsBus"
+                label="Bus"
+              />
+            </div>
+          </div>
+        </q-card-section>
 
+        <q-separator />
+
+        <q-card-actions align="right">
+          <q-btn flat label="Cancelar" color="primary" v-close-popup />
+          <q-btn flat label="Aceptar" color="primary" @click="editarTicket()" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <div>
+      <q-table title="Ticket" :rows="rows" :columns="columns" row-key="name">
+        <template v-slot:body-cell-estado="props">
+          <q-td :props="props">
+            <label for="" v-if="props.row.estado == 1" style="color: green"
+              >Activo</label
+            >
+            <label for="" v-else style="color: red">Inactivo</label>
+          </q-td>
+        </template>
+        <template v-slot:body-cell-opciones="props">
+          <q-td :props="props" class="botones">
+            <q-btn
+              color="white"
+              text-color="black"
+              label="🖋️"
+              @click="EditarTicket(props.row._id)"
+            />
+            <q-btn
+              glossy
+              label="❌"
+              @click="InactivarTicket(props.row._id)"
+              v-if="props.row.estado == 1"
+            />
+            <q-btn
+              glossy
+              label="✔️"
+              @click="ActivarTicket(props.row._id)"
+              v-else
+            />
+          </q-td>
+        </template>
+      </q-table>
     </div>
+  </div>
 </template>
-  
+
 <script setup>
-import axios from 'axios';
-import { ref, onMounted } from 'vue';
-import { format } from 'date-fns';
-import { useTicketStore } from '../stores/Tickete.js';
-import { useVendedorStore } from '../stores/Vendedor.js';
-import { useClienteStore } from '../stores/Cliente.js';
-import { useRutaStore } from '../stores/Ruta.js';
-import { useBusStore } from '../stores/Bus.js';
-const TicketStore = useTicketStore()
-const VendedorStore = useVendedorStore()
-const clienteStore = useClienteStore()
-const rutaStore = useRutaStore()
-const busStore = useBusStore()
+import axios from "axios";
+import { ref, onMounted } from "vue";
+import { format } from "date-fns";
+import { useTicketStore } from "../stores/Tickete.js";
+import { useVendedorStore } from "../stores/Vendedor.js";
+import { useClienteStore } from "../stores/Cliente.js";
+import { useRutaStore } from "../stores/Ruta.js";
+import { useBusStore } from "../stores/Bus.js";
+const TicketStore = useTicketStore();
+const VendedorStore = useVendedorStore();
+const clienteStore = useClienteStore();
+const rutaStore = useRutaStore();
+const busStore = useBusStore();
+let Nmro_ticket = ref("");
+let fecha_venta = ref("");
+let Vendedor_id = ref("");
+let Cliente_id = ref("");
+let Ruta_id = ref("");
+let Transporte_id = ref("");
+let optionsVendedor = ref([]);
+let optionsCliente = ref([]);
+let optionsBus = ref([]);
+let optionsRutas = ref([]);
 /* const conductorStore = useConductorStore() */
-let buses = ref([]);
+let ticketes = ref([]);
 let rows = ref([]);
-let fixed = ref(false)
-let Vehiculo = ref('');
+let fixed = ref(false);
+let Vehiculo = ref("");
 let NumAsientos = ref();
-let conductor_id = ref('');
-let cambio = ref(0)
+let conductor_id = ref("");
+let cambio = ref(0);
 
 async function obtenerInfo() {
-    try {
-        await busStore.obtenerInfoBuses();
-        console.log(busStore.buses);
-        buses.value = busStore.buses;
-        rows.value = busStore.buses;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await TicketStore.obtenerInfoTicket();
+    console.log(TicketStore.ticketes);
+    ticketes.value = TicketStore.ticketes;
+    rows.value = TicketStore.ticketes;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const options = ref([
-    'google', 'api'
-])
+const options = ref(["google", "api"]);
 onMounted(async () => {
-    obtenerInfo()
+  obtenerInfo();
 });
 
 const columns = [
-    { name: 'Nmro_ticket', label: 'Numero de ticket', field: 'Nmro_ticket', sortable: true },
-    { name: 'NumAsientos', label: 'Numero De Asientos', field: 'NumAsientos', sortable: true },
-    { name: 'conductor_id', label: 'Conductor', field: (row) => row.conductor_id.nombre },
-    { name: 'estado', label: 'Estado', field: 'estado', sortable: true, format: (val) => (val ? 'Activo' : 'Inactivo') },
-    {
-        name: 'opciones', label: 'Opciones',
-        field: row => null,
-        "sortable": false,
-    },
+  {
+    name: "Nmro_ticket",
+    label: "Numero de ticket",
+    field: "Nmro_ticket",
+    sortable: true,
+  },
+  {
+    name: "fecha_venta",
+    label: "Fecha de venta",
+    field: "fecha_venta",
+    sortable: true,
+    format: (val) => format(new Date(val), "yyyy-MM-dd"),
+  },
+  {
+    name: "Vendedor_id",
+    label: "Vendedor",
+    field: (row) => `${row.Vendedor_id.Nombre}`,
+  },
+  {
+    name: "Cliente_id",
+    label: "Cliente",
+    field: (row) => `${row.Cliente_id.Nombre_cliente}`,
+  },
+  { name: "Ruta_id", label: "Ruta", field: (row) => `${row.Ruta_id.codigo}` },
+  {
+    name: "Transporte_id",
+    label: "Bus",
+    field: (row) => `${row.Transporte_id.NumBus}`,
+  },
+  {
+    name: "estado",
+    label: "Estado",
+    field: "estado",
+    sortable: true,
+    format: (val) => (val ? "Activo" : "Inactivo"),
+  },
+  {
+    name: "opciones",
+    label: "Opciones",
+    field: (row) => null,
+    sortable: false,
+  },
 ];
+async function obtenerVendedor() {
+  try {
+    await VendedorStore.obtenerInfoVendedor();
+    optionsVendedor.value = VendedorStore.vendedores.map((vendedor) => ({
+      label: `${vendedor.Nombre} `,
+      value: String(vendedor._id),
+    }));
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function obtenerCliente() {
+  try {
+    await clienteStore.obtenerInfoCliente();
+    optionsCliente.value = clienteStore.clientes.map((cliente) => ({
+      label: `${cliente.nombre}`,
+      value: String(cliente._id),
+    }));
+  } catch (error) {
+    console.log(error);
 
-function agregarBus() {
-    obtenerConductor();
-    fixed.value = true;
-    cambio.value = 0
+
+  }
+}
+async function obtenerBuses() {
+  try {
+    await rutaStore.obtenerInfoRutas();
+    const ruta_ids = rutaStore.rutas.map((ruta) => String(ruta._id));
+    await busStore.obtenerInfoBuses();
+    const busesFiltrados = busStore.buses.filter((bus) =>
+      ruta_ids.includes(String(bus.ruta_id._id))
+    );
+    optionsBus.value = busesFiltrados.map((bus) => ({
+      label: `${bus.NumBus}`,
+      value: String(bus._id),
+    }));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-async function editarAgregarBus() {
-    if (cambio.value === 0) {
-        await busStore.postBus({
-
-            Vehiculo: Vehiculo.value,
-            NumAsientos: NumAsientos.value,
-            conductor_id: conductor_id.value,
-        })
-        limpiar()
-        obtenerInfo()
-
-    } else {
-        let id = idBus.value;
-        if (id) {
-            await busStore.putEditarBus(id, {
-                Vehiculo: Vehiculo.value,
-                conductor_id: conductor_id.value,
-                NumAsientos: NumAsientos.value,
-            });
-
-            limpiar();
-            obtenerInfo()
-            fixed.value = false;
-        }
-    }
+async function editarTicket() {
+  let id = idTicket.value;
+  if (id) {
+    await TicketStore.putEditarTicket(id, {
+      Vendedor_id: Vendedor_id._rawValue.value,
+      Cliente_id: Cliente_id._rawValue.value,
+      Transporte_id: Transporte_id._rawValue.value,
+      Ruta_id: Ruta_id._rawValue.value,
+      Nmro_ticket: Nmro_ticket.value,
+      fecha_venta: fecha_venta.value,
+    });
+    limpiar();
+    obtenerInfo();
+    fixed.value = false;
+  }
 }
 
-
-async function obtenerConductor() {
-    try {
-        await conductorStore.obtenerInfoConductor();
-        options.value = conductorStore.conductores.map((conductor) => (
-            {
-                label: `${conductor.nombre}`,
-                value: String(conductor._id)
-            }));
-    } catch (error) {
-        console.log(error);
-    }
-}
+/* async function obtenerConductor() {
+  try {
+    await conductorStore.obtenerInfoConductor();
+    options.value = conductorStore.conductores.map((conductor) => ({
+      label: `${conductor.nombre}`,
+      value: String(conductor._id),
+    }));
+  } catch (error) {
+    console.log(error);
+  }
+} */
 function limpiar() {
-    Vehiculo.value = ""
-    NumAsientos.value = ""
-    conductor_id = ""
+  Nmro_ticket.value = "";
+  fecha_venta.value = "";
+  Vendedor_id.value = "";
+  Cliente_id.value = "";
+  Ruta_id.value = "";
+  Transporte_id.value = "";
 }
 
-let idBus = ref('')
-async function EditarBus(id) {
-    obtenerConductor();
-    const busSeleccionado = buses.value.find((transporte) => transporte._id === id);
-    if (busSeleccionado) {
-        idBus.value = String(busSeleccionado._id);
-        fixed.value = true;
-        text.value = "Editar Bus";
-        conductor_id.value = busSeleccionado.conductor_id;
-        Vehiculo.value = busSeleccionado.Vehiculo;
-        NumAsientos.value = busSeleccionado.NumAsientos;
-    }
+let idTicket = ref("");
+async function EditarTicket(id) {
+  await obtenerCliente();
+  await obtenerVendedor();
+  await obtenerBuses();
+  const TicketSeleccionado = ticketes.value.find((ticket) => ticket._id === id);
+  if (TicketSeleccionado) {
+    idTicket.value = String(TicketSeleccionado._id);
+    fixed.value = true;
+    text.value = "Editar Bus";
+
+    Vendedor_id.value = {
+      label: `${TicketSeleccionado.Vendedor_id.Nombre}`,
+      value: String(TicketSeleccionado.Vendedor_id._id),
+    };
+
+    cliente.value = {
+      label: `${TicketSeleccionado.cliente_id.Nombre_cliente}`,
+      value: String(TicketSeleccionado.cliente_id._id),
+    };
+
+    Transporte_id.value = {
+      label: ` N°${TicketSeleccionado.Transporte_id.NumBus}`,
+      value: String(TicketSeleccionado.Transporte_id._id),
+    };
+
+    Nmro_ticket.value = Nmro_ticket.value;
+
+    const date = new Date(TicketSeleccionado.fecha_venta);
+    const formattedDate = date.toISOString().split("T")[0];
+
+    fecha_venta.value = formattedDate;
+  }
 }
 
-async function InactivarBus(id) {
-    await busStore.putInactivarBus(id)
-    obtenerInfo()
+/* let idTicket = ref("");
+async function EditarTicket(id) {
+  await obtenerCliente();
+  await obtenerVendedor();
+  await obtenerBuses();
+  const ticketSelect = tickets.value.find((ticket) => ticket._id === id);
+  if (ticketSelect) {
+    idTicket.value = String(ticketSelect._id);
+    fixed.value = true;
+    text.value = "Editar Ticket";
+    vendedor.value = {
+      label: `${ticketSelect.vendedor_id.nombre} - ${ticketSelect.vendedor_id.telefono}`,
+      value: String(ticketSelect.vendedor_id._id),
+    };
+
+    cliente.value = {
+      label: `${ticketSelect.cliente_id.nombre} - ${ticketSelect.cliente_id.cedula} - ${ticketSelect.cliente_id.telefono}`,
+      value: String(ticketSelect.cliente_id._id),
+    };
+
+    bus.value = {
+      label: `${ticketSelect.bus_id.empresa_asignada} - ${ticketSelect.bus_id.placa} - N°${ticketSelect.bus_id.numero_bus}`,
+      value: String(ticketSelect.bus_id._id),
+    };
+    no_asiento.value = ticketSelect.no_asiento;
+
+    const date = new Date(ticketSelect.fecha_departida);
+    const formattedDate = date.toISOString().split("T")[0];
+
+    fecha_departida.value = formattedDate;
+  }
+}
+ */
+async function InactivarTicket(id) {
+  await busStore.putInactivarTicket(id);
+  obtenerInfo();
 }
 
-async function ActivarBus(id) {
-    await busStore.putActivarBus(id)
-    obtenerInfo()
+async function ActivarTicket(id) {
+  await busStore.putActivarTicket(id);
+  obtenerInfo();
 }
-
-
 </script>
-  
+
 <style scoped>
 .modal-content {
-    width: 400px;
+  width: 400px;
 }
 
 .botones button {
-    margin: 2px;
+  margin: 2px;
 }
 
 .btn-agregar {
-    width: 100%;
-    margin-bottom: 5px;
-    display: flex;
-    justify-content: center;
-    color: black;
+  width: 100%;
+  margin-bottom: 5px;
+  display: flex;
+  justify-content: center;
+  color: black;
 }
 </style>
